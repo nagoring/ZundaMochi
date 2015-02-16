7e7b45ee739c5ce75c446402a33b6ef4415e8096
nagomi <nagoling@gmail.com>
2015-02-16 22:23:59 +0900

    カッコの位置変更

--- app/src/Model/Entity/PluginSetting.php
+++ app/src/Model/Entity/PluginSetting.php
@@ -6,16 +6,15 @@ use Cake\ORM\Entity;
 /**
  * PluginSetting Entity.
  */
-class PluginSetting extends Entity
-{
+class PluginSetting extends Entity {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        'name' => true,
-        'priority' => true,
-    ];
+	/**
+	 * Fields that can be mass assigned using newEntity() or patchEntity().
+	 *
+	 * @var array
+	 */
+	protected $_accessible = [
+		'name' => true,
+		'priority' => true,
+	];
 }
