82abd8c3831e93754a35318a9e5b8e6687572345
nagomi <nagomi@nagomi-pc-core7>
2015-09-21 21:10:31 +0900

    /m/co****に対応

--- app/plugins/Community/config/routes.php
+++ app/plugins/Community/config/routes.php
@@ -3,5 +3,17 @@ use Cake\Routing\Router;
 
 Router::plugin('Community', function ($routes) {
 //    $routes->fallbacks('InflectedRoute');
+//    $routes->connect('/', ['controller' => 'Communities', 'action' => 'index', 'Plugin' => 'Community']);
     $routes->fallbacks('DashedRoute');
+});
+
+Router::scope('/m', ['plugin' => 'Community'], function ($routes) {
+    $routes->connect(
+		'/co:id', 
+		['controller' => 'Communities', 'action' => 'view'],
+		[
+			'pass' => 'id',
+			':id' => '[0-9]+',
+		]
+	);
 });
