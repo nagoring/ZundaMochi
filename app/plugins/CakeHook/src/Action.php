82f01624891e2cda18eded62e8b6fd104638ee02
nagomi <nagomi@nagomi-pc-core7>
2015-09-22 17:34:22 +0900

    メソッドのコメント追加

--- app/plugins/CakeHook/src/Action.php
+++ app/plugins/CakeHook/src/Action.php
@@ -8,6 +8,15 @@ namespace CakeHook;
 class Action {
 	protected static $_queue = [];
 	protected static $_maxIndex = 255;
+	/**
+	 * 登録されたアクションの呼び出し
+	 * groupとactionを指定して登録したアクションを逐次実行する
+	 * 
+	 * @param string $group
+	 * @param string $action
+	 * @param type $param
+	 * @return type
+	 */
 	public static function action($group, $action, $param){
 		if(!isset(self::$_queue[$group][$action]))return;
 		ksort(self::$_queue[$group][$action]);
@@ -20,6 +29,20 @@ class Action {
 		}
 		return $state->getReturn();
 	}
+	/**
+	 * アクションの登録を行う
+	 * \CakeHook\Controller\CakeHookAppControllerを継承しているコントローラーなら自動でコントローラーのアクションになる
+	 * アクションとはラベルを貼って関数を遅延実行するための機能である
+	 * actionメソッドによる呼び出しで初めて実行される
+	 * indexはデフォルトが100になり、少ないほうが先に実行される
+	 * group,action,indexが一致した場合次の空いているindexを登録する
+	 * @param string $group
+	 * @param string $action
+	 * @param int $index
+	 * @param type $func
+	 * @return type
+	 * @throws \Exception
+	 */
 	public static function add($group, $action, $index, $func){
 		if(!isset(self::$_queue[$group])) self::$_queue[$group] = [];
 		if(!isset(self::$_queue[$group][$action])) self::$_queue[$group][$action] = [];
@@ -37,11 +60,30 @@ class Action {
 			self::$_queue[$group][$action][$index] = $func;
 		}
 	}
+	/**
+	 * アクションの上書きを行う
+	 * group,action,indexが一致した場合そこを上書きする
+	 * 空の場合はそのまま追加される
+	 * @param type $group
+	 * @param type $action
+	 * @param type $index
+	 * @param type $func
+	 */
 	public static function overwrite($group, $action, $index, $func){
 		if(!isset(self::$_queue[$group])) self::$_queue[$group] = [];
 		if(!isset(self::$_queue[$group][$action])) self::$_queue[$group][$action] = [];
 		self::$_queue[$group][$action][$index] = $func;
 	}
+	/**
+	 * アクションの削除を行う
+	 * groupのみを指定した場合そのgroupのアクションは全て削除される
+	 * groupとactionを指定した場合はactionのアクションは全て削除される
+	 * groupとactionとindexを指定した場合は指定したindexのアクションを削除する
+	 * @param string $group
+	 * @param string $action
+	 * @param int $index
+	 * @return type
+	 */
 	public static function remove($group, $action = null, $index = null){
 		if($index !== null){
 			unset(self::$_queue[$group][$action][$index]);
@@ -53,6 +95,12 @@ class Action {
 		}
 		unset(self::$_queue[$group]);
 	}
+	/**
+	 * 指定したアクションが存在するか調べる
+	 * @param string $group
+	 * @param string $action
+	 * @return type
+	 */
 	public static function is($group, $action){
 		return isset(self::$_queue[$group][$action]);
 	}
--- app/plugins/CakeHook/src/State.php
+++ app/plugins/CakeHook/src/State.php
@@ -10,24 +10,50 @@ class State {
 	private $param;
 	private $return;
 	private $_this;
+	/**
+	 * 指定したパラメータを渡す
+	 * 必ず_thisを含む。_thisは呼び出し元のコントローラー
+	 * @param array $param
+	 * @eaxample
+	 *	[
+	 *		'aaa' => 100,
+	 *		'_this' => $this(コントローラー)
+	 *	]
+	 */
 	public function __construct($param) {
 		$this->param = $param;
 		$this->_this = $param['_this'];
 		unset($this->param['_this']);
 	}
-	
+	/**
+	 * Hookされている次のアクションに渡すための返り値をセットする
+	 * @param type $return
+	 */
 	public function setReturn($return){
 		$this->return = $return;
 	}
+	/**
+	 * 前の呼びだされたHookされているアクションの返り値を受け取る
+	 * @return type
+	 */
 	public function getReturn(){
 		return $this->return;
 	}
+	/**
+	 * Hookされたアクションに渡された引数を取得する
+	 * @return type
+	 */
 	public function getParam(){
 		return $this->param;
 	}
+	/**
+	 * 呼び出し元のコントローラーの取得
+	 * @return type
+	 */
 	public function getThis(){
 		return $this->_this;
 	}
+	
 	public function getArgs(){
 		return $this->param['pass'];
 	}
