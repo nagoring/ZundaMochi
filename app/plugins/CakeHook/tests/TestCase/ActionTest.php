ee3732dd8bcbf30333b4c848af3f64bc8aa7b072
nagomi <nagomi@nagomi-pc-core7>
2015-09-23 03:25:37 +0900

    Actionのテスト追加

--- app/plugins/CakeHook/tests/TestCase/ActionTest.php
+++ app/plugins/CakeHook/tests/TestCase/ActionTest.php
@@ -10,12 +10,15 @@ use Cake\TestSuite\IntegrationTestCase;
  */
 class ActionTest extends IntegrationTestCase
 {
+	public function setUp() {
+		parent::setUp();
+		Action::clear();
+	}
     /**
      *
      * @return void
      */
-    public function testAddSimple()
-    {
+    public function testAddSimple(){
 		$group = 'ActionTestController';
 		$action = 'sayHello';
 		$index = 10;
@@ -24,8 +27,7 @@ class ActionTest extends IntegrationTestCase
 		});
 		$this->assertSame('hello', Action::action($group, $action, []));
     }
-    public function testAddMultiIndexes()
-    {
+    public function testAddMultiIndexes_pt1(){
 		$group = 'ActionTestController';
 		$action = 'sayHello';
 		$index = 10;
@@ -38,4 +40,82 @@ class ActionTest extends IntegrationTestCase
 		});
 		$this->assertSame('hello world', Action::action($group, $action, []));
     }
+	/**
+	 * 同じindexを指定した場合自動で次のindexで登録されているか
+	 */
+    public function testAddMultiIndexes_pt2(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return 'hello';
+		});
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return $state->getReturn() . ' world';
+		});
+		$this->assertSame('hello world', Action::action($group, $action, []));
+    }
+    public function testAddMaximumIndex(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 1000000;
+		try{
+			Action::add($group, $action, $index, function(\CakeHook\State $state){
+				return 'hello';
+			});
+			$this->fail('PHPUnit fail');
+		} catch (\Exception $ex) {
+			$this->assertSame('Failed addAction over max index :' . $index, $ex->getMessage());
+		}
+    }
+	public function testActionWithParam(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			$param = $state->getParam();
+			$name = $param['name'];
+			return $name . ':hello';
+		});
+		$this->assertSame('yamada:hello', Action::action($group, $action, ['name' => 'yamada']));
+	}
+    public function testOverwrite(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return 'hello';
+		});
+		$index = 10;
+		Action::overwrite($group, $action, $index, function(\CakeHook\State $state){
+			return $state->getReturn() . ' world';
+		});
+		$this->assertSame(' world', Action::action($group, $action, []));
+    }
+    public function testRemove(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return 'hello';
+		});
+		$index = 11;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return $state->getReturn() . ' world';
+		});
+		Action::remove($group, $action, 11);
+		$this->assertSame('hello', Action::action($group, $action, []));
+    }
+    public function testIs(){
+		$group = 'ActionTestController';
+		$action = 'sayHello';
+		$index = 10;
+		Action::add($group, $action, $index, function(\CakeHook\State $state){
+			return 'hello';
+		});
+		$this->assertTrue(Action::is($group, $action));
+    }
 }
+
+
