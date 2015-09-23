1ccbeaae2a64d8533e2f44abad76efaa344249fa
nagomi <nagomi@nagomi-pc-core7>
2015-09-24 00:25:12 +0900

    Table呼び出し補助関数追加

--- app/plugins/DefaultZundaMochi/config/function.php
+++ app/plugins/DefaultZundaMochi/config/function.php
@@ -1,4 +1,5 @@
 <?php
+use Cake\ORM\TableRegistry;
 
 function checkAttackImage($image){
 	mb_regex_encoding('ASCII');
@@ -12,5 +13,9 @@ function checkAttackImage($image){
 	if (preg_match('/<\\?php./i', $image)) {
 		die('Attack detected');
 	}	
+}
+function getTableModel($class_name, $class_path){
+	$config = TableRegistry::exists($class_name) ? [] : ['className' => $class_path];
+	return TableRegistry::get($class_name, $config);
 }
 
