baab8b57ac0cc1534279d6e4ea24d4bc5e355f62
nagomi <nagoling@gmail.com>
2015-10-09 02:19:16 +0900

    added comment

--- app/plugins/CakeHook/src/Filter.php
+++ app/plugins/CakeHook/src/Filter.php
@@ -4,6 +4,12 @@ namespace CakeHook;
 class Filter {
 	protected static $_queue = [];
 	protected static $_maxIndex = 120;
+	/**
+	 * execute addtional filter
+	 * @param string $label
+	 * @param array $param
+	 * @return type
+	 */
 	public static function filter($label,  $param = null){
 		if(!isset(self::$_queue[$label]))return $param;
 		ksort(self::$_queue[$label]);
@@ -14,6 +20,15 @@ class Filter {
 		}
 		return $state->getReturn();
 	}
+	/**
+	 * Add filter. 
+	 * This method was registerd, it would be execute by filter method
+	 * @param string $label
+	 * @param int $index
+	 * @param \Closure $func
+	 * @return void
+	 * @throws \Exception
+	 */
 	public static function add($label, $index, $func){
 		if(!isset(self::$_queue[$label])) self::$_queue[$label] = [];
 		if(isset(self::$_queue[$label][$index])){
@@ -29,10 +44,24 @@ class Filter {
 			self::$_queue[$label][$index] = $func;
 		}
 	}
+	/**
+	 * Overwrite registered function by the new function.
+	 * @param string $label
+	 * @param int $index
+	 * @param \Closure $func
+	 */
 	public static function overwrite($label, $index, $func){
 		if(!isset(self::$_queue[$label])) self::$_queue[$label] = [];
 		self::$_queue[$label][$index] = $func;
 	}
+	/**
+	 * Remove registered function.
+	 * 1. Target index function was remove
+	 * 2. Target index is null.  Remove all index in label.
+	 * @param string $label
+	 * @param int $index
+	 * @return void
+	 */
 	public static function remove($label, $index = null){
 		if($index !== null){
 			unset(self::$_queue[$label][$index]);
@@ -40,8 +69,19 @@ class Filter {
 		}
 		unset(self::$_queue[$label]);
 	}
+	/**
+	 * This method check whether be registered method.
+	 * @param string $label
+	 * @return boolean
+	 */
 	public static function is($label){
 		return isset(self::$_queue[$label]);
+	}
+	/**
+	 * clear the method that has been registered
+	 */
+	public static function clear(){
+		unset(self::$_queue);
 	}
 }
 
